0a121ad498f235d1a2af1857875badf36bc202ce
mahdirezaie336 <mahdi.rezaie.336@gmail.com>
2022-12-08 12:53:09 +0330

    Add salt

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
 import sys
+import os
+import pbkdf2
 
-# Get args
+# Read the key from the file
+with open('./secret.key', 'r') as f:
+    key = f.read().strip()
+
+    # Add 16 byte salt using os lib
+    key = key.encode() + os.urandom(16)
+
+    # Expand the key to 256 bits
+    key = pbkdf2.PBKDF2(key).read(32)
+
 
